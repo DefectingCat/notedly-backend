c578b4d5a8690289e34a8d7fa1688eedf893d3e0
DefectingCat <i@defect.ink>
2021-08-06 17:23:31 +0800

    update: gravatar mirror

--- src/util/gravatar.ts
+++ src/util/gravatar.ts
@@ -4,7 +4,7 @@ import md5 from 'md5';
 
 const gravatar = (email: string): string => {
   const hash = md5(email);
-  return `https://www.gravatar.com/avatar/${hash}.jpg?d=identicon`;
+  return `https://cn.gravatar.com/avatar/${hash}.jpg?d=identicon`;
 };
 
 export default gravatar;
